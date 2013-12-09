c1d2b73a083afacb4860395b0dcab78788365947
Bart van Delft <vandeba@chalmers.se>
2013-12-09 16:45:32 +0100

    Removed old sub file

--- src/Language/Java/Paragon/Sub.hs
+++
@@ -1,6 +0,0 @@
-module Language.Java.Paragon.Sub
-where
-
--- | Just testing if Haddock finds this stuff.
-data MyData = Something Int    -- ^ Integer stuff
-              | Otherthing Bool -- ^ Boolean stuff
