72acb3850457cb4b1c9c46eb916870d466990143
Dmytro Lypai <dmytrolypai@gmail.com>
2014-01-11 17:26:09 +0100

    Reorganization of Parser tests.

--- test/Language/Java/Paragon/ParserSpec.hs
+++ test/Language/Java/Paragon/ParserSpec.hs
@@ -5,8 +5,11 @@
 -- | Parser tests.
 module Language.Java.Paragon.ParserSpec (main, spec) where
 
+import System.FilePath ((</>), (<.>))
 import Test.Hspec
 import Text.ParserCombinators.Parsec.Error
+import Control.Monad (liftM2)
+import Control.Applicative ((<$>))
 
 import Language.Java.Paragon.Syntax
 import Language.Java.Paragon.Parser
@@ -23,29 +26,37 @@ instance Eq ParseError where
 instance Eq Message where
   (==) = messageEq
 
+-- Configuration
+
+testDir :: FilePath
+testDir = "test" </> "Language" </> "Java" </> "Paragon" </> "parser"
+
+successDir :: FilePath
+successDir = testDir </> "success"
+
+failureDir :: FilePath
+failureDir = testDir </> "failure"
+
 -- | Main specification function.
 spec :: Spec
 spec = do
   describe "parse" $ do
     -- Success
     it "parses an empty program" $
-      let cuSrcSpan = SrcSpan "empty" 1 1 1 1 in
-      parse "" "empty"
-        `shouldBe`
-      Right (CompilationUnit cuSrcSpan Nothing [] [])
+      let fileName = "Empty.para"
+          cuSrcSpan = SrcSpan fileName 1 1 1 1
+      in successCase fileName (CompilationUnit cuSrcSpan Nothing [] [])
 
     it "parses package declaration with single identifier" $
-      let fileName = "PkgDecl"
+      let fileName = "PkgDeclSingle.para"
           cuSrcSpan = SrcSpan fileName 1 1 1 16
           pdSrcSpan = SrcSpan fileName 1 1 1 16
           qIdSrcSpan = SrcSpan fileName 1 9 1 15
-          paragonId = Id (SrcSpan fileName 1 9 1 15) "paragon" in
-      parse "package paragon;" fileName
-        `shouldBe`
-      Right (CompilationUnit cuSrcSpan (Just $ PackageDecl pdSrcSpan (Name qIdSrcSpan paragonId PkgName Nothing)) [] [])
+          paragonId = Id (SrcSpan fileName 1 9 1 15) "paragon"
+      in successCase fileName (CompilationUnit cuSrcSpan (Just $ PackageDecl pdSrcSpan (Name qIdSrcSpan paragonId PkgName Nothing)) [] [])
 
-    it "parses package declaration with qualified identifier" $
-      let fileName = "PkgDecl"
+    it "parses package declaration with qualified name" $
+      let fileName = "PkgDeclQName.para"
           cuSrcSpan = SrcSpan fileName 1 1 1 28
           pdSrcSpan = SrcSpan fileName 1 1 1 28
           qIdSrcSpan = SrcSpan fileName 1 9 1 27
@@ -53,35 +64,30 @@ spec = do
           seName = Name (SrcSpan fileName 1 9 1 10) seId PkgName Nothing
           chalmersId = Id (SrcSpan fileName 1 12 1 19) "chalmers"
           chalmersName = Name (SrcSpan fileName 1 9 1 19) chalmersId PkgName (Just seName)
-          paragonId = Id (SrcSpan fileName 1 21 1 27) "paragon" in
-      parse "package se.chalmers.paragon;" fileName
-        `shouldBe`
-      Right (CompilationUnit cuSrcSpan (Just $ PackageDecl pdSrcSpan (Name qIdSrcSpan paragonId PkgName (Just chalmersName))) [] [])
+          paragonId = Id (SrcSpan fileName 1 21 1 27) "paragon"
+      in successCase fileName (CompilationUnit cuSrcSpan (Just $ PackageDecl pdSrcSpan (Name qIdSrcSpan paragonId PkgName (Just chalmersName))) [] [])
+
     -- TODO: fix name types
     it "parses single type import declaration" $
-      let fileName = "ImportDecl"
+      let fileName = "ImportDeclSingle.para"
           cuSrcSpan = SrcSpan fileName 1 1 1 22
           impdSrcSpan = SrcSpan fileName 1 1 1 22
           qIdSrcSpan = SrcSpan fileName 1 8 1 21
           policyId = Id (SrcSpan fileName 1 16 1 21) "Policy"
           paragonName = Name (SrcSpan fileName 1 8 1 14) paragonId AmbigName Nothing
-          paragonId = Id (SrcSpan fileName 1 8 1 14) "paragon" in
-      parse "import paragon.Policy;" fileName
-        `shouldBe`
-      Right (CompilationUnit cuSrcSpan Nothing [SingleTypeImport impdSrcSpan (Name qIdSrcSpan policyId AmbigName (Just paragonName))] [])
+          paragonId = Id (SrcSpan fileName 1 8 1 14) "paragon"
+      in successCase fileName (CompilationUnit cuSrcSpan Nothing [SingleTypeImport impdSrcSpan (Name qIdSrcSpan policyId AmbigName (Just paragonName))] [])
 
     it "parses import declaration for all the types in a package" $
-      let fileName = "ImportDecl"
+      let fileName = "ImportDeclTypeOnDemand.para"
           cuSrcSpan = SrcSpan fileName 1 1 1 17
           impdSrcSpan = SrcSpan fileName 1 1 1 17
           qIdSrcSpan = SrcSpan fileName 1 8 1 14
-          paragonId = Id (SrcSpan fileName 1 8 1 14) "paragon" in
-      parse "import paragon.*;" fileName
-        `shouldBe`
-      Right (CompilationUnit cuSrcSpan Nothing [TypeImportOnDemand impdSrcSpan (Name qIdSrcSpan paragonId AmbigName Nothing)] [])
+          paragonId = Id (SrcSpan fileName 1 8 1 14) "paragon"
+      in successCase fileName (CompilationUnit cuSrcSpan Nothing [TypeImportOnDemand impdSrcSpan (Name qIdSrcSpan paragonId AmbigName Nothing)] [])
 
     it "parses static import declaration of a single type" $
-      let fileName = "ImportDecl"
+      let fileName = "ImportDeclSingleStatic.para"
           cuSrcSpan = SrcSpan fileName 1 1 1 27
           impdSrcSpan = SrcSpan fileName 1 1 1 27
           qIdSrcSpan = SrcSpan fileName 1 15 1 26
@@ -89,181 +95,134 @@ spec = do
           mathName = Name (SrcSpan fileName 1 15 1 23) mathId AmbigName (Just langName)
           mathId = Id (SrcSpan fileName 1 20 1 23) "Math"
           langName = Name (SrcSpan fileName 1 15 1 18) langId AmbigName Nothing
-          langId = Id (SrcSpan fileName 1 15 1 18) "lang" in
-      parse "import static lang.Math.PI;" fileName
-        `shouldBe`
-      Right (CompilationUnit cuSrcSpan Nothing [SingleStaticImport impdSrcSpan (Name qIdSrcSpan piId AmbigName (Just mathName))] [])
+          langId = Id (SrcSpan fileName 1 15 1 18) "lang"
+      in successCase fileName (CompilationUnit cuSrcSpan Nothing [SingleStaticImport impdSrcSpan (Name qIdSrcSpan piId AmbigName (Just mathName))] [])
 
     it "parses static import declaration for all members" $
-      let fileName = "ImportDecl"
+      let fileName = "ImportDeclStaticOnDemand.para"
           cuSrcSpan = SrcSpan fileName 1 1 1 26
           impdSrcSpan = SrcSpan fileName 1 1 1 26
           qIdSrcSpan = SrcSpan fileName 1 15 1 23
           mathId = Id (SrcSpan fileName 1 20 1 23) "Math"
           langName = Name (SrcSpan fileName 1 15 1 18) langId AmbigName Nothing
-          langId = Id (SrcSpan fileName 1 15 1 18) "lang" in
-      parse "import static lang.Math.*;" fileName
-        `shouldBe`
-      Right (CompilationUnit cuSrcSpan Nothing [StaticImportOnDemand impdSrcSpan (Name qIdSrcSpan mathId AmbigName (Just langName))] [])
+          langId = Id (SrcSpan fileName 1 15 1 18) "lang"
+      in successCase fileName (CompilationUnit cuSrcSpan Nothing [StaticImportOnDemand impdSrcSpan (Name qIdSrcSpan mathId AmbigName (Just langName))] [])
 
     it "parses semicolon type declaration" $
-      let fileName = "SemiColonDecl"
-          cuSrcSpan = SrcSpan fileName 1 1 1 1 in
-      parse ";" fileName
-        `shouldBe`
-      Right (CompilationUnit cuSrcSpan Nothing [] [])
+      let fileName = "SemiColonDecl.para"
+          cuSrcSpan = SrcSpan fileName 1 1 1 1
+      in successCase fileName (CompilationUnit cuSrcSpan Nothing [] [])
 
     it "parses empty class declaration" $
-      let fileName = "ClassDecl"
+      let fileName = "ClassDeclEmpty.para"
           cuSrcSpan = SrcSpan fileName 1 1 1 10
           ctdSrcSpan = SrcSpan fileName 1 1 1 10
           cdSrcSpan = SrcSpan fileName 1 1 1 10
           cbSrcSpan = SrcSpan fileName 1 9 1 10
           cd = ClassDecl cdSrcSpan [] cId [] Nothing [] (ClassBody cbSrcSpan [])
-          cId = Id (SrcSpan fileName 1 7 1 7) "C" in
-      parse "class C {}" fileName
-        `shouldBe`
-      Right (CompilationUnit cuSrcSpan Nothing [] [ClassTypeDecl ctdSrcSpan cd])
+          cId = Id (SrcSpan fileName 1 7 1 7) "C"
+      in successCase fileName (CompilationUnit cuSrcSpan Nothing [] [ClassTypeDecl ctdSrcSpan cd])
 
     it "parses empty interface declaration" $
-      let fileName = "InterfaceDecl"
+      let fileName = "InterfaceDeclEmpty.para"
           cuSrcSpan = SrcSpan fileName 1 1 1 14
           inttdSrcSpan = SrcSpan fileName 1 1 1 14
           intdSrcSpan = SrcSpan fileName 1 1 1 14
           intd = InterfaceDecl intdSrcSpan [] intId [] [] IB
-          intId = Id (SrcSpan fileName 1 11 1 11) "I" in
-      parse "interface I {}" fileName
-        `shouldBe`
-      Right (CompilationUnit cuSrcSpan Nothing [] [InterfaceTypeDecl inttdSrcSpan intd])
+          intId = Id (SrcSpan fileName 1 11 1 11) "I"
+      in successCase fileName (CompilationUnit cuSrcSpan Nothing [] [InterfaceTypeDecl inttdSrcSpan intd])
 
     it "parses empty class declaration with modifiers" $
-      let fileName = "ClassDecl"
+      let fileName = "ClassDeclMod.para"
           cuSrcSpan = SrcSpan fileName 1 1 1 17
           ctdSrcSpan = SrcSpan fileName 1 1 1 17
           cdSrcSpan = SrcSpan fileName 1 1 1 17
           pblSrcSpan = SrcSpan fileName 1 1 1 6
           cbSrcSpan = SrcSpan fileName 1 16 1 17
           cd = ClassDecl cdSrcSpan [Public pblSrcSpan] cId [] Nothing [] (ClassBody cbSrcSpan [])
-          cId = Id (SrcSpan fileName 1 14 1 14) "C" in
-      parse "public class C {}" fileName
-        `shouldBe`
-      Right (CompilationUnit cuSrcSpan Nothing [] [ClassTypeDecl ctdSrcSpan cd])
+          cId = Id (SrcSpan fileName 1 14 1 14) "C"
+      in successCase fileName (CompilationUnit cuSrcSpan Nothing [] [ClassTypeDecl ctdSrcSpan cd])
 
     it "parses empty interface declaration with modifiers" $
-      let fileName = "InterfaceDecl"
+      let fileName = "InterfaceDeclMod.para"
           cuSrcSpan = SrcSpan fileName 1 1 1 21
           inttdSrcSpan = SrcSpan fileName 1 1 1 21
           intdSrcSpan = SrcSpan fileName 1 1 1 21
           pblSrcSpan = SrcSpan fileName 1 1 1 6
           intd = InterfaceDecl intdSrcSpan [Public pblSrcSpan] intId [] [] IB
-          intId = Id (SrcSpan fileName 1 18 1 18) "I" in
-      parse "public interface I {}" fileName
-        `shouldBe`
-      Right (CompilationUnit cuSrcSpan Nothing [] [InterfaceTypeDecl inttdSrcSpan intd])
+          intId = Id (SrcSpan fileName 1 18 1 18) "I"
+      in successCase fileName (CompilationUnit cuSrcSpan Nothing [] [InterfaceTypeDecl inttdSrcSpan intd])
 
     -- Failure
-    context "given a package declaration with missing semicolon" $
-      it "gives an error message" $
-        let Left err = parse "package paragon" "PkgDecl"
-        in show err `shouldBe` "\"PkgDecl\" (line 1, column 9):\n\
-                                \unexpected end of input\n\
-                                \expecting . or ;"
+    describe "gives an error message when" $ do
 
-    context "given a package declaration with missing package name" $
-      it "gives an error message" $
-        let Left err = parse "package ;" "PkgDecl"
-        in show err `shouldBe` "\"PkgDecl\" (line 1, column 9):\n\
-                                \unexpected ;\n\
-                                \expecting package name"
+      it "given a package declaration with missing semicolon" $
+        failureCase "PkgDeclMissSemiColon"
 
-    context "given a package declaration with a typo in package keyword" $
-      it "gives an error message" $
-        let Left err = parse "packag paragon;" "PkgDecl"
-        in show err `shouldBe` "\"PkgDecl\" (line 1, column 1):\n\
-                                \unexpected packag\n\
-                                \expecting package declaration, import declarations, type declarations or end of input"
+      it "given a package declaration with missing package name" $
+        failureCase "PkgDeclMissPkgName"
 
-    context "given an import declaration with missing semicolon" $
-      it "gives an error message" $
-        let Left err = parse "import paragon" "ImportDecl"
-        in show err `shouldBe` "\"ImportDecl\" (line 1, column 8):\n\
-                                \unexpected end of input\n\
-                                \expecting . or ;"
+      it "given a package declaration with a typo in package keyword" $
+        failureCase "PkgDeclKeywordTypo"
 
-    context "given an import declaration with missing package name" $
-      it "gives an error message" $
-        let Left err = parse "import ;" "ImportDecl"
-        in show err `shouldBe` "\"ImportDecl\" (line 1, column 8):\n\
-                                \unexpected ;\n\
-                                \expecting keyword static or package/type name"
+      it "given an import declaration with missing semicolon" $
+        failureCase "ImportDeclMissSemiColon"
 
-    context "given an unfinished import declaration" $
-      it "gives an error message" $
-        let Left err = parse "import paragon.;" "ImportDecl"
-        in show err `shouldBe` "\"ImportDecl\" (line 1, column 16):\n\
-                                \unexpected ;\n\
-                                \expecting * or identifier"
+      it "given an import declaration with missing package name" $
+        failureCase "ImportDeclMissPkgName"
 
-    context "given a class declaration with missing name" $
-      it "gives an error message" $
-        let Left err = parse "class {}" "ClassDecl"
-        in show err `shouldBe` "\"ClassDecl\" (line 1, column 7):\n\
-                                \unexpected {\n\
-                                \expecting class name"
+      it "given an unfinished import declaration" $
+        failureCase "ImportDeclUnfinished"
 
-    context "given a class declaration with missing opening brace" $
-      it "gives an error message" $
-        let Left err = parse "class C }" "ClassDecl"
-        in show err `shouldBe` "\"ClassDecl\" (line 1, column 9):\n\
-                                \unexpected }\n\
-                                \expecting class body"
+      it "given a class declaration with missing name" $
+        failureCase "ClassDeclMissName"
 
-    context "given a class declaration with missing closing brace" $
-      it "gives an error message" $
-        let Left err = parse "class C {" "ClassDecl"
-        in show err `shouldBe` "\"ClassDecl\" (line 1, column 9):\n\
-                                \unexpected end of input\n\
-                                \expecting }"
+      it "given a class declaration with missing opening brace" $
+        failureCase "ClassDeclMissOpenBrace"
 
-    context "given a class declaration with missing body" $
-      it "gives an error message" $
-        let Left err = parse "class C" "ClassDecl"
-        in show err `shouldBe` "\"ClassDecl\" (line 1, column 7):\n\
-                                \unexpected end of input\n\
-                                \expecting class body"
+      it "given a class declaration with missing closing brace" $
+        failureCase "ClassDeclMissCloseBrace"
 
-    context "given an interface declaration with missing name" $
-      it "gives an error message" $
-        let Left err = parse "interface {}" "InterfaceDecl"
-        in show err `shouldBe` "\"InterfaceDecl\" (line 1, column 11):\n\
-                                \unexpected {\n\
-                                \expecting interface name"
+      it "given a class declaration with missing body" $
+        failureCase "ClassDeclMissBody"
 
-    context "given an interface declaration with missing opening brace" $
-      it "gives an error message" $
-        let Left err = parse "interface I }" "InterfaceDecl"
-        in show err `shouldBe` "\"InterfaceDecl\" (line 1, column 13):\n\
-                                \unexpected }\n\
-                                \expecting {"
+      it "given an interface declaration with missing name" $
+        failureCase "InterfaceDeclMissName"
 
-    context "given an interface declaration with missing closing brace" $
-      it "gives an error message" $
-        let Left err = parse "interface I {" "InterfaceDecl"
-        in show err `shouldBe` "\"InterfaceDecl\" (line 1, column 13):\n\
-                                \unexpected end of input\n\
-                                \expecting }"
+      it "given an interface declaration with missing opening brace" $
+        failureCase "InterfaceDeclMissOpenBrace"
 
-    context "given a class declaration with misspelled modifier" $
-      it "gives an error message" $
-        let Left err = parse "privat class C {}" "ClassDecl"
-        in show err `shouldBe` "\"ClassDecl\" (line 1, column 1):\n\
-                                \unexpected privat\n\
-                                \expecting package declaration, import declarations, type declarations or end of input"
+      it "given an interface declaration with missing closing brace" $
+        failureCase "InterfaceDeclMissCloseBrace"
 
-    context "given an interface declaration with misspelled modifier" $
-      it "gives an error message" $
-        let Left err = parse "publc interface I {}" "InterfaceDecl"
-        in show err `shouldBe` "\"InterfaceDecl\" (line 1, column 1):\n\
-                                \unexpected publc\n\
-                                \expecting package declaration, import declarations, type declarations or end of input"
+      it "given a class declaration with misspelled modifier" $
+        failureCase "ClassDeclModTypo"
+
+      it "given an interface declaration with misspelled modifier" $
+        failureCase "InterfaceDeclModTypo"
+
+-- Infrastructure
+
+successCase :: String -> AST SrcSpan -> IO ()
+successCase fileName result = do
+  input <- successRead fileName
+  parse input fileName `shouldBe` Right result
+
+successRead :: String -> IO String
+successRead fileName = readFile (successDir </> fileName)
+
+failureCase :: String -> IO ()
+failureCase baseName = do
+  (input, errMsg) <- failureRead baseName
+  let Left err = parse input (baseName <.> "para")
+  show err `shouldBe` errMsg
+
+failureRead :: String -> IO (String, String)
+failureRead baseName = liftM2 (,) (readFile (failureDir </> baseName <.> "para"))
+                                  (dropNewLine <$> readFile (failureDir </> baseName <.> "err"))
+  where dropNewLine ""  = ""
+        dropNewLine str = let l = last str
+                          in if l == '\n' || l == '\r'
+                               then dropNewLine (init str)
+                               else str
 
