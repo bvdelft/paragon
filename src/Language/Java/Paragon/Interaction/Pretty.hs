c9276055b914d4643bf06f88ae872fb1f1a25774
Dmytro Lypai <dmytrolypai@gmail.com>
2014-03-23 19:47:22 +0100

    Add comments. See Issue #4.

--- src/Language/Java/Paragon/Interaction/Pretty.hs
+++ src/Language/Java/Paragon/Interaction/Pretty.hs
@@ -1,6 +1,7 @@
 -- | Defines the pretty printing type class, plus some general instances for
 -- this type class. To avoid circular dependencies, all other instances should
 -- be defined in the module defining the data type to be pretty printed.
+-- Pretty printing is used for debugging (cleaned up representation).
 module Language.Java.Paragon.Interaction.Pretty
   (
     -- * The @Pretty@ type class.
--- src/Language/Java/Paragon/Interaction/Unparse.hs
+++ src/Language/Java/Paragon/Interaction/Unparse.hs
@@ -1,6 +1,7 @@
 -- | Defines the 'unparsing' type class, plus some general instances for
 -- this type class. To avoid circular dependencies, all other instances should
--- be defined in the module defining the data type to be pretty printed.
+-- be defined in the module defining the data type to be unparsed.
+-- Unparsing is used in error messages and code generation.
 module Language.Java.Paragon.Interaction.Unparse
   (
     -- * The @Unparse@ type class.
