bc228a0065f5839bd5272d31d0ff5d51e78fd0a7
Bart van Delft <vandeba@chalmers.se>
2014-03-28 14:41:40 +0100

    Removed SrcSpan argument from test - SyntaxSpec

--- test/Language/Java/Paragon/SyntaxSpec.hs
+++ test/Language/Java/Paragon/SyntaxSpec.hs
@@ -3,6 +3,7 @@ module Language.Java.Paragon.SyntaxSpec (main, spec) where
 
 import Test.Hspec
 
+import Language.Java.Paragon.Annotation
 import Language.Java.Paragon.Syntax
 
 -- | To be able to run this module from GHCi.
@@ -14,13 +15,12 @@ spec :: Spec
 spec = do
   describe "ann" $ do
     it "returns annotation for leaf node" $
-      ann (Null "ann") `shouldBe` "ann"
+      ann (Null emptyAnnotation) `shouldBe` emptyAnnotation
 
     it "returns annotation for internal node (1 level)" $
-      ann (Lit (Int "ann" 2)) `shouldBe` "ann"
+      ann (Lit (Int emptyAnnotation 2)) `shouldBe` emptyAnnotation
 
-    it "returns annotation for internal node (2 levels)" $
-      ann (RefType (ClassRefType (ClassType "ann" (Name "annWrong" (Id "annWrong" "Object") TypeName Nothing) [])))
-        `shouldBe`
-      "ann"
-
+    it "returns annotation for internal node (2 levels)" $ do
+      let wrongAnnotation = emptyAnnotation { annIsNull = False }
+      ann (RefType (ClassRefType (ClassType emptyAnnotation (Name wrongAnnotation (Id wrongAnnotation "Object") TypeName Nothing) [])))
+        `shouldBe` emptyAnnotation
