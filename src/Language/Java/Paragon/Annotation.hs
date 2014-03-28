81eef95f22a20d076c2e6046d422e7ca2e177e7a
Bart van Delft <vandeba@chalmers.se>
2014-03-28 15:57:09 +0100

    Special Eq instance for Annotation.

--- src/Language/Java/Paragon/Annotation.hs
+++ src/Language/Java/Paragon/Annotation.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BangPatterns #-}
+
 -- | Annotation data structure. Paragon uses the same annotation through-out the
 -- entire compilation. Elements of the annotation are gradually filled by each
 -- compilation phase.
@@ -8,6 +10,10 @@ module Language.Java.Paragon.Annotation
   , emptyAnnotation
   , srcSpanToAnn
   ) where
+
+import Control.Exception (catch, SomeException)
+import System.IO.Unsafe (unsafePerformIO)
+import Prelude hiding (catch)
 
 import Language.Java.Paragon.Interaction.Panic
 import Language.Java.Paragon.SrcPos
@@ -23,7 +29,32 @@ data Annotation = Annotation
   , annType     :: Maybe (TcType, Bool)
     -- | Whether the value of this node can be null.
   , annIsNull   :: Bool
-  } deriving (Show, Eq, Ord)
+  } deriving (Show, Ord)
+
+-- | Since every part of an annotation that has not been instantiated
+-- yet (i.e. the phase that instantiates this part has not yet passed)
+-- gives a @panic@, equality comparisons in early phases of the compiler
+-- could give panics if we derived @Eq@ - in particular during testing.
+-- Therefore we implement @Eq@ here, using @unsafePerformIO@ to consider
+-- two attributes equal when they both give the same panic.
+instance Eq Annotation where
+  (==) annA annB =  panicSafeEq (annSrcSpan annA) (annSrcSpan annB)
+                 && panicSafeEq (annType    annA) (annType    annB)
+                 && panicSafeEq (annIsNull  annA) (annIsNull  annB)
+   where panicSafeEq :: Eq a => a -> a -> Bool
+         panicSafeEq x y = safeEval x == safeEval y
+         -- | Guarantees to give a value. Either the intended value, but
+         -- if it was an error message then this error message as
+         -- string. Even better would be to have a special data type
+         -- instead of a String, but safe here since none of the
+         -- annotations is a String.
+         safeEval :: Eq a => a -> Either String a
+         safeEval val = unsafePerformIO $ 
+                          catch ( do let !_forceEval = val
+                                     return $ Right val ) 
+                                (\err -> return $ Left $ show (err::SomeException))
+
+           
 
 -- | Empty annotation. Any evaluation of an element of this annotation will
 -- result in a panic.
@@ -33,7 +64,7 @@ emptyAnnotation = Annotation
   , annType    = panic thisFunction $ msg "type"
   , annIsNull  = panic thisFunction $ msg "nullpointer"
   }
-  where thisFunction = "Language.Java.Paragon.Annotation.defaultAnnotation"
+  where thisFunction = "Language.Java.Paragon.Annotation.emptyAnnotation"
         msg str = "The " ++ str ++ " annotation is not available in this phase."
 
 srcSpanToAnn :: SrcSpan -> Annotation
